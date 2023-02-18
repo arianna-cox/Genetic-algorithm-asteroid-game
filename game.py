I rotation fixed yay

game.py
@@ -151,7 +151,7 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self, surface):
         # Draw the spaceship to the given surface
-        image = pygame.transform.rotate(self.image, self.angle - np.pi / 2)
+        image = pygame.transform.rotate(self.image, np.degrees(self.angle - np.pi / 2))
         rect = image.get_rect()
         rect.center = self.rect.center
         surface.blit(image, rect)
@@ -377,8 +377,8 @@ class Game():
             NUMBER_OF_SECTORS = int(len(inputs)/3)
             SECTOR_EDGES = np.linspace(-np.pi, np.pi, NUMBER_OF_SECTORS + 1)
             for n in range(NUMBER_OF_SECTORS):
-                pygame.draw.line(screen, (255, 255, 255), self.player.position, unit_vector(self.player.angle + SECTOR_EDGES[n]) * 600 + self.player.position, width=1)
-                pygame.draw.line(screen, (0, 255, 0), self.player.position, unit_vector(self.player.angle + inputs[n+2]) * 600 + self.player.position)
+                pygame.draw.line(screen, (255, 255, 255), self.player.position, unit_vector(self.player.angle + SECTOR_EDGES[n]) * 1200 + self.player.position, width=1)
+                pygame.draw.line(screen, (0, 255, 0), self.player.position, unit_vector(self.player.angle + inputs[3*n+2]) * 1200 + self.player.position)
 
         # Update content of the screen
         pygame.display.flip()

input_translator.py
@@ -10,7 +10,8 @@ def soonest_to_hit(player, asteroids, NUMBER_OF_SECTORS):
     # 3. the angle of the asteroid in the frame of reference of the player (in the range -pi to pi)
 
     # Finds the asteroid with the highest 1/(time until impact) in each sector
-    soonest_to_hit_asteroids = np.array([(0, np.pi, 0) for _ in range(NUMBER_OF_SECTORS)])
+    SECTOR_EDGES = np.linspace(-np.pi, np.pi, NUMBER_OF_SECTORS + 1)
+    soonest_to_hit_asteroids = np.array([(0, np.pi, (SECTOR_EDGES[i]+SECTOR_EDGES[i+1])/2) for i in range(NUMBER_OF_SECTORS)])
     for asteroid in asteroids:
         # Find the time until impact given relative radial speed of the asteroid and the player
         relative_velocity = asteroid.velocity - player.velocity
@@ -25,12 +26,10 @@ def soonest_to_hit(player, asteroids, NUMBER_OF_SECTORS):
 
         # Determine which sector the asteroid is in
         sector_number = 0
-        # Should sector edges be inside the function or outside??
-        SECTOR_EDGES = np.linspace(-np.pi, np.pi, NUMBER_OF_SECTORS + 1)
         while relative_angle > SECTOR_EDGES[sector_number + 1]:
             sector_number += 1
         # Check which 1/(time until impact) is highest in this sector
-        if soonest_to_hit_asteroids[sector_number][0] < inverse_time_to_impact:
+        if soonest_to_hit_asteroids[sector_number][0] <= inverse_time_to_impact:
             # Find the angle between the asteroids' relative velocity and the line joining the asteroid and the player
             angle_relative_velocity = range_pi_to_pi(find_angle(relative_velocity) - find_angle(-displacement))
             soonest_to_hit_asteroids[sector_number] = (inverse_time_to_impact, angle_relative_velocity, relative_angle)